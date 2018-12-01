689a319bef83c9401ee6012142cc1af32c572fdd
Felix <stenbackfelix@gmail.com>
2018-12-01 22:12:08 +0100

    December 1 puzzle

---
+++ dec1/code.py
@@ -0,0 +1,31 @@
+
+
+
+with open('input.txt', 'r') as f:
+    current_freq = 0
+    freq_dict = {}
+    lines = []
+    freq_dict[current_freq] = True
+    found = False
+
+    # Store lines for re-use
+    for line in f:
+        lines.append(line)
+
+
+    while not found:
+
+        for line in lines:
+
+            change_in_freq = int(line)
+            current_freq += change_in_freq
+
+            if current_freq in freq_dict:
+                found = True
+                print('Seen before,', current_freq)
+                break
+
+            else:
+                print('Not seen before,', current_freq)
+                freq_dict[current_freq] = True
+    #print(t)
