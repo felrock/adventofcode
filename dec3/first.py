5b92095dd9713d78270cabf4b845810d5d5986d7
Felix <stenbackfelix@gmail.com>
2018-12-03 06:49:19 +0100

    December 3rd challenge

---
+++ dec3/first.py
@@ -0,0 +1,27 @@
+
+
+
+with open('input.txt', 'r') as f:
+
+    claims = []
+    fabric = [ [0 for _ in range(1000)] for _ in range(1000) ]
+    total = 0
+    for claim in f:
+        claims.append(claim)
+
+    for claim in claims:
+
+        sizeStr = claim.split(': ')[-1].split('x')
+        sizeInt = (int(sizeStr[0]), int(sizeStr[1]))
+        coordStr = claim.split('@ ')[-1].split(': ')[0].split(',')
+        x, y = (int(coordStr[0]), int(coordStr[1]))
+
+        for iy in range(y, y+sizeInt[1]):
+            for ix in range(x, x+sizeInt[0]):
+                   fabric[iy][ix] += 1
+    for col in fabric:
+        for item in col:
+            if item >= 2:
+                total += 1
+    print(total)
+
---
+++ dec3/second.py
@@ -0,0 +1,39 @@
+
+with open('input.txt', 'r') as f:
+
+    claims = []
+    fabric = [ [0 for _ in range(1000)] for _ in range(1000) ]
+    total = 0
+
+    for claim in f:
+        claims.append(claim)
+
+    # Apply claims to the fabric
+    for claim in claims:
+
+        sizeStr = claim.split(': ')[-1].split('x')
+        sizeInt = (int(sizeStr[0]), int(sizeStr[1][:-1]))
+        coordStr = claim.split('@ ')[-1].split(': ')[0].split(',')
+        x, y = (int(coordStr[0]), int(coordStr[1]))
+
+        for iy in range(y, y+sizeInt[1]):
+            for ix in range(x, x+sizeInt[0]):
+                fabric[iy][ix] += 1
+
+    # Search the fabric for claims that doesn't overlap, where
+    # fabric[y][x] == 1 for the entire claim
+    for claim in claims:
+
+        sizeStr = claim.split(': ')[-1].split('x')
+        sizeInt = (int(sizeStr[0]), int(sizeStr[1][:-1]))
+        coordStr = claim.split('@ ')[-1].split(': ')[0].split(',')
+        x, y = (int(coordStr[0]), int(coordStr[1]))
+        found = True
+
+        for iy in range(y, y+sizeInt[1]):
+            for ix in range(x, x+sizeInt[0]):
+                if fabric[iy][ix] > 1:
+                    found = False
+        if found:
+            print(claim, end='')
+            break
